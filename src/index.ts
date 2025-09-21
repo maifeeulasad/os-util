665849e9e17050bbcf94ff9963017660a9b1df12
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-09-21 07:11:48 +0600

    [feature]: basic exports;

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { NetworkMonitor } from './NetworkMonitor.js';
+export { SpeedFormatter, DisplayMode } from './SpeedFormatter.js';
+export { ConfigManager } from './ConfigManager.js';
+export type { NetworkStats, SpeedStats } from './NetworkMonitor.js';
+export type { Config } from './ConfigManager.js';
